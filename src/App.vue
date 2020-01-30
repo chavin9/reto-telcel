2659c1bc32908be44200e625de39ead4c13a2304
Alan Dega <arkidega@gmail.com>
2020-01-30 16:53:14 -0600

    c2

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
     <h1>hola vue</h1>
     <table style="width:100%">
   <tr v-for="radiobase in radiobases" :key="id">
+
     <th>{{radiobase}}</th>
+    <td v-for="radiobase in radiobases" :key="id">
+         {{radiobase}}</td>
   </tr>
+  
+  
   
 
     
@@ -31,14 +36,15 @@ export default {
       esMenor40_90: null,
       mayor_90: null,
       radiobases: [],
-      elementos: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+      trafico:[]
     }
   },
   mounted(){
     axios.get('http://localhost:4000/telcel')
     .then(res => {
       console.log('res',res)
-      this.radiobases = res.data
+      this.radiobases = res.data[2]
+      this.trafico = res.data[1]
       console.log('radiobases', this.radiobases)
     })
   },
